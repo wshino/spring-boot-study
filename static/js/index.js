5470114a676da47a18f192084fbc9cbfab8c0fee
shinohara wataru <shinohara@thewoody.jp>
2015-01-18 16:17:57 +0900

    make scaffold that try to vue.

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    title: 'todos',
+    todos: [
+      {
+        done: true,
+        content: 'Learn JavaScript'
+      },
+      {
+        done: false,
+        content: 'Learn Vue.js'
+      }
+    ]
+  }
+})
+console.log("hogehoge");
